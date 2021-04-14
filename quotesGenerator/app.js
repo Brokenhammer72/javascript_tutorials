9b77422004472ae4c16ddaa445829a8dff219034
amey patil <ameywpatil@gmail.com>
2021-04-14 19:18:15 +0530

    quotesGenerator

---
+++ quotesGenerator/app.js
@@ -0,0 +1,17 @@
+function newQuote() {
+	fetch('https://api.quotable.io/random')
+		.then(response => {
+			return response.json();
+		})
+		.then((data) =>{
+			var quote = data.content;
+			var author = data.author;
+			var length = data.length; 
+			if(length > 250){
+				newQuote();
+			}
+			document.getElementById('quote-text').innerHTML= quote;
+			document.getElementById('author-name').innerHTML= author;
+			console.log('hello');
+		})
+}
